2fe9d68506ec4ed1b129f265d623595e4374890d
augus <augus@DELL-AUGUSTO>
2026-04-12 18:23:59 -0300

    Etapa 3: Adicao de metadados ProducesResponseType no VeiculosController

--- LocadoraVeiculosTP1/Controllers/VeiculosController.cs
+++ LocadoraVeiculosTP1/Controllers/VeiculosController.cs
@@ -2,9 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using LocadoraVeiculosTP1.Data;
 using LocadoraVeiculosTP1.Models;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace LocadoraVeiculosTP1.Controllers
 {
@@ -20,12 +17,15 @@ namespace LocadoraVeiculosTP1.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Veiculo>), StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<Veiculo>>> GetVeiculos()
         {
             return await _context.Veiculos.Include(v => v.Fabricante).Include(v => v.Categoria).ToListAsync();
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Veiculo), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Veiculo>> GetVeiculo(int id)
         {
             var veiculo = await _context.Veiculos.FindAsync(id);
@@ -34,6 +34,9 @@ namespace LocadoraVeiculosTP1.Controllers
         }
 
         [HttpPost]
+        [Consumes("application/json")]
+        [ProducesResponseType(typeof(Veiculo), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Veiculo>> PostVeiculo(Veiculo veiculo)
         {
             if (veiculo.AnoFabricacao < 1900 || veiculo.AnoFabricacao > DateTime.Now.Year + 1)
@@ -45,19 +48,24 @@ namespace LocadoraVeiculosTP1.Controllers
         }
 
         [HttpPut("{id}")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PutVeiculo(int id, Veiculo veiculo)
         {
-            if (id != veiculo.Id) return BadRequest();
+            if (id != veiculo.Id) return BadRequest("ID incompatível.");
             _context.Entry(veiculo).State = EntityState.Modified;
             await _context.SaveChangesAsync();
             return NoContent();
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteVeiculo(int id)
         {
             var veiculo = await _context.Veiculos.FindAsync(id);
-            if (veiculo == null) return NotFound();
+            if (veiculo == null) return NotFound("Veículo não encontrado.");
             _context.Veiculos.Remove(veiculo);
             await _context.SaveChangesAsync();
             return NoContent();
